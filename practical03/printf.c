f1d2606bebb6dbbc66d94cd949cb093d904cf0b7
masterclara <clara.zehe@ucdconnect.ie>
2021-10-06 14:28:37 +0100

    Minor corrections to comments and text output in printf.c

--- practical03/printf.c
+++ practical03/printf.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    /* declare integer and floating point variables */
+    /* declare two integer and floating point variables */
     int int1, int2;
     float float1, float2;
 
-    /*Initialise variables */
+    /*Assign values */
     int1 = 4;
     int2 = 17;
 
@@ -18,8 +18,8 @@ int main(void)
     /*Printing variable values */
 
     /*default format */
-    printf("Default: Two ints %d, %d and two floats %f, %f\n", int1, int2, float1, float2);
+    printf("Default: Two ints %d %d and two floats %f %f\n", int1, int2, float1, float2);
 
     /* floats with 4 decimal places */
-    printf("Four decimal floats: Two ints %d, %d and two floats %.4f, %4f\n", int1, int2, float1, float2);
+    printf("Four decimal floats: Two ints %d %d and two floats %.4f %4f\n", int1, int2, float1, float2);
 }
