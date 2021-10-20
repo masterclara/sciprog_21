186923abdd19a7b5f2854ed80c40ffb4dade0a3a
masterclara <clara.zehe@ucdconnect.ie>
2021-10-20 20:28:48 +0100

    fibonacci.c source code completed and tested on sciprog.

--- practical05/fibonacci.c
+++ practical05/fibonacci.c
@@ -1,3 +1,5 @@
+// compile in sciprog using gcc -o fibonacci fibonacci.c
+
 #include <stdio.h>
 
 int userinput(void);
@@ -6,11 +8,7 @@ int fibonacci(int fn1, int fn2);
 int main(void)
 {
     // declare variables
-    int n, f0, f1;
-
-    // assign initial values
-    f0 = 0;
-    f1 = 1;
+    int n, i, j;
 
     // request user input of n
     n = userinput();
@@ -22,10 +20,16 @@ int main(void)
     Fib[1] = 1;
 
     // Fibonacci series loop
-    for (int i = 2; i <= n; i++)
+    for (i = 2; i <= n; i++)
     {
         Fib[i] = fibonacci(Fib[i - 1], Fib[i - 2]);
-        printf("T[%d] = %f (f(x) at n = %d)\n", i, Fib[i], i);
+    }
+
+    // print fibonacci series
+    printf("The Fibonacci series F(n) up to n = %d is:\n", n);
+    for (j = 0; j <= n; j++)
+    {
+        printf("F[%d] = %d at n = %d\n", j, Fib[j], j);
     }
 }
 
@@ -42,5 +46,5 @@ int fibonacci(int fn1, int fn2)
 {
     int Fn = fn1 + fn2;
 
-    return Fn, fn1;
+    return Fn;
 }
