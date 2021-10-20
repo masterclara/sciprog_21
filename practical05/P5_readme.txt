18b6174500d12940ab0c33d3a3a7ef7df9664be5
masterclara <clara.zehe@ucdconnect.ie>
2021-10-20 20:41:12 +0100

    initial commit for hyperarctan.c and updated readme file

--- practical05/P5_readme.txt
+++ practical05/P5_readme.txt
@@ -2,3 +2,4 @@ This is the folder for Scientific Programming Concepts - Practical 5
 
 It contains the following source code file(s):
 fibonacci.c	- creates program to display the Fibonacci series up to n (user input)
+hyperarctan.c	- creates program to calculate hyperbolic arc tangent through Maclaurin series and natural logarithms and compares the accuracy of both methods
---
+++ practical05/hyperarctan.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+// function declaration
+float artanh1(float x);
+float artanh2(float x);
+
+int main(void)
+{
+    float d;
+
+    d = userinput();
+}
+
+// function to request user input of delta
+float userinput(void)
+{
+    float delta;
+    printf("Please enter a real number < 1 here:\n");
+    scanf("%f", &delta);
+    return delta;
+}
+
+float artanh1(float x)
+{
+}
+
+float artanh2(float x)
+{
+}
